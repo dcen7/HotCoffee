155891c054b365a71a7c1215f7b7b8acaf11e961
dcen7 <mdcengiz@gmail.com>
2023-04-10 12:40:18 -0700

    setup interface

---
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddOrderViewController.swift
+//  HotCoffee
+//
+//  Created by Deniz Cengiz on 4/10/23.
+//
+
+import Foundation
+import UIKit
+
+class AddOrderViewController: UIViewController {
+    
+}
---
+++ HotCoffee/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrdersTableViewController.swift
+//  HotCoffee
+//
+//  Created by Deniz Cengiz on 4/10/23.
+//
+
+import Foundation
+import UIKit
+
+class OrderTableViewController: UITableViewController {
+    
+}
